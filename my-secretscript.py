0a7dde919eaf9b87732471f0644f1d493382dc5e
selychan <93843083+selychan@users.noreply.github.com>
2024-07-24 17:40:03 +0300

    Create my-secretscript.py
    create the alert for gitleaks

---
+++ my-secretscript.py
@@ -0,0 +1,11 @@
+# my_secret_script.py
+
+def main():
+    # This is a mock secret API key for demonstration purposes
+    secret_api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+    
+    # Just print the secret key to simulate its usage
+    print(f"Using secret API key: {secret_api_key}")
+
+if __name__ == "__main__":
+    main()
