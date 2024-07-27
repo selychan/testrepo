ad3e802405ca491c747de54573135a9cb9ca36f8
selychan <93843083+selychan@users.noreply.github.com>
2024-07-27 22:53:17 +0300

    update files

--- app.py
+++ app.py
@@ -9,5 +9,6 @@ def index():
         name = request.form.get('name')
     return render_template('index.html', name=name)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
